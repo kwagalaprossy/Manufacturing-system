53f14e76059d3227c572a322794924834302e290
PatiencePatricia <amusolopatiencepatricia@gmail.com>
2022-10-23 19:05:22 +0300

    editing the admin dashboard
    removing the admin rights of adding distributors

--- Admin/dashboard.php
+++ Admin/dashboard.php
@@ -230,7 +230,7 @@ include"config.php";
 						<li>
 							<a href="javascript: void(0);" class="has-arrow waves-effect"><i class="fas fa-users fa-3x"></i> <span>Distributors</span> </a>
 							<ul class="sub-menu" aria-expanded="false">
-								<li><a href="add_distributors.php">Add Distributors</a></li>
+								
 								<li><a href="distributors.php">View Distributors</a></li>
 								
 							</ul>

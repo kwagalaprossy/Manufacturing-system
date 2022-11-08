115597f3c10337c21abce6455302af4477bca660
kwagalaprossy <kwagalaprossy67@gmail.com>
2022-11-07 21:04:36 -0800

    csv
    csv download

--- wholesaler/manpro.php
+++ wholesaler/manpro.php
@@ -380,7 +380,9 @@
                                     <?php 
                                  } ?>
                                 </table>
-                                <button class="btn btn-primary" onclick="tableToCSV()">Export to CSV</button>
+                              <!--   <button class="btn btn-primary" onclick="tableToCSV()">
+                                Export to CSV</button> -->
+                                <button type="button" onclick="exportTableToCSV('products.csv')" class="btn btn-primary" style="padding-bottom: 5px; float:left;"><i class="fa fa-download" style=" padding:5px;"></i>Export to Csv</button>
                             </div>
                         </div>
                     </div>
@@ -478,7 +480,7 @@
 
     <!-- init js -->
     <script src="assets/js/pages/ecommerce-datatables.init.js"></script>
-    <script>
+   <!--  <script>
     $(function () {
       $("#example1").DataTable({
         "responsive": true,
@@ -494,7 +496,45 @@
         "responsive": true,
       });
     });
+  </script> -->
+   <script>
+    //user-defined function to download CSV file  
+    function downloadCSV(csv, filename) {
+      var csvFile;
+      var downloadLink;
+
+      //define the file type to text/csv  
+      csvFile = new Blob([csv], {
+        type: 'text/csv'
+      });
+      downloadLink = document.createElement("a");
+      downloadLink.download = filename;
+      downloadLink.href = window.URL.createObjectURL(csvFile);
+      downloadLink.style.display = "none";
+
+      document.body.appendChild(downloadLink);
+      downloadLink.click();
+    }
+
+    //user-defined function to export the data to CSV file format  
+    function exportTableToCSV(filename) {
+      //declare a JavaScript variable of array type  
+      var csv = [];
+      var rows = document.querySelectorAll("table tr");
+
+      //merge the whole data in tabular form   
+      for (var i = 0; i < rows.length; i++) {
+        var row = [],
+          cols = rows[i].querySelectorAll("td, th");
+        for (var j = 0; j < cols.length; j++)
+          row.push(cols[j].innerText);
+        csv.push(row.join(","));
+      }
+      //call the function to download the CSV file  
+      downloadCSV(csv.join("\n"), filename);
+    }
   </script>
+
 
 </body>
 

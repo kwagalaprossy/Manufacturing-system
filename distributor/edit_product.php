b76641c3b623a8dfad8f5a4be7d48523952d1d7a
Patrene <norahpatrene@gmail.com>
2022-11-10 08:09:12 -0800

    edit_profile added

--- distributor/edit_product.php
+++ distributor/edit_product.php
@@ -326,7 +326,7 @@
 					<div class="dropdown d-inline-block">
 						<button type="button" class="btn header-item waves-effect" id="page-header-user-dropdown" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false"> <img class="rounded-circle header-profile-user" src="#" alt=""> <span class="d-none d-xl-inline-block ms-1 fw-medium font-size-15">Distributor</span> <i class="uil-angle-down d-none d-xl-inline-block font-size-15"></i> </button>
 						<div class="dropdown-menu dropdown-menu-end">
-							<!-- item--><a class="dropdown-item" href="change_password.php"><i
+							<!-- item--><a class="dropdown-item" href="edit_profile.php"><i
 									class="uil uil-user-circle font-size-18 align-middle text-muted me-1"></i> <span
 									class="align-middle">View Profile</span></a> 
 									<a class="dropdown-item" href="logout.php">

8e3f5187cc8e61064f31c11e168e1e46af3d82e1
kwagalaprossy <kwagalaprossy67@gmail.com>
2022-11-10 23:03:29 -0800

    Update invoice.php
    changes in invoice.php for retailer

--- retailer/invoice.php
+++ retailer/invoice.php
@@ -466,6 +466,7 @@
                                             <?php } ?>
                                         </tbody>
                                     </table>
+                                            <button type="button" onclick="exportTableToCSV('products.csv')" class="btn btn-primary" style="padding-bottom: 5px; float:left;"><i class="fa fa-download" style=" padding:5px;"></i>Export to Csv</button>
                                 </div>
                         </div>
                     </div>
@@ -574,6 +575,43 @@
         "responsive": true,
       });
     });
+  </script>
+   <script>
+    //user-defined function to download CSV file  
+    function downloadCSV(csv, filename) {
+      var csvFile;
+      var downloadLink;
+
+      //define the file type to text/csv  
+      csvFile = new Blob([csv], {
+        type: 'text/csv'
+      });
+      downloadLink = document.createElement("a");
+      downloadLink.download = filename;
+      downloadLink.href = window.URL.createObjectURL(csvFile);
+      downloadLink.style.display = "none";
+
+      document.body.appendChild(downloadLink);
+      downloadLink.click();
+    }
+
+    //user-defined function to export the data to CSV file format  
+    function exportTableToCSV(filename) {
+      //declare a JavaScript variable of array type  
+      var csv = [];
+      var rows = document.querySelectorAll("table tr");
+
+      //merge the whole data in tabular form   
+      for (var i = 0; i < rows.length; i++) {
+        var row = [],
+          cols = rows[i].querySelectorAll("td, th");
+        for (var j = 0; j < cols.length; j++)
+          row.push(cols[j].innerText);
+        csv.push(row.join(","));
+      }
+      //call the function to download the CSV file  
+      downloadCSV(csv.join("\n"), filename);
+    }
   </script>
 </body>
 
